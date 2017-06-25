72b8fd205669620ef2df265cfa5aaaa613f6a4bc
Christopher Hannah <christopher.hannah@me.com>
2017-06-25 16:49:36 +0100

    arc4random substitute for Linux

---
+++ Sources/Utils.swift
@@ -0,0 +1,21 @@
+//
+//  Utils.swift
+//  TextCase-API
+//
+//  Created by Christopher Hannah on 25/06/2017.
+//
+//
+
+import Foundation
+
+// arc4random substitute for Linux
+//
+// https://gist.github.com/rymcol/48a505c2a1c874daea52a296a2687f5f
+
+#if os(Linux)
+	import SwiftGlibc
+	
+	public func arc4random_uniform(_ max: UInt32) -> Int32 {
+  return (SwiftGlibc.rand() % Int32(max-1))
+	}
+#endif
