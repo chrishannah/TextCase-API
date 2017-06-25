1b6221c20d9af74d55f22d92e55af69a8f0cc983
Christopher Hannah <christopher.hannah@me.com>
2017-06-25 14:40:56 +0100

    Basic server implementation.

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,23 @@
-print("Hello, world!")
+import PerfectLib
+import PerfectHTTP
+import PerfectHTTPServer
+
+let server = HTTPServer()
+server.serverPort = 8080
+
+var routes = Routes()
+routes.add(method: .get, uri: "/", handler: {
+	request, response in
+	response.setBody(string: "Hello, Perfect!")
+		.completed()
+})
+
+server.addRoutes(routes)
+
+do {
+	try server.start()
+} catch PerfectError.networkError(let error, let message) {
+	print("Network error: ")
+	print("    \(error)")
+	print("    \(message)")
+}
